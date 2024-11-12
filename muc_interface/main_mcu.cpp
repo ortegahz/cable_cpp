ed066ea7f06af2386a1417bb94887d9a69232b1d
huangzhe <huangzhe@jbufa.com>
2024-11-12 13:40:52 +0800

    update main_mcu.cpp for time control

--- muc_interface/main_mcu.cpp
+++ muc_interface/main_mcu.cpp
@@ -10,12 +10,7 @@
 #define RAW_DATA_ONE_GROUP_LENGTH 64
 using namespace std;
 
-int constructInputData()
-{
-    return 0;
-}
-
-std::vector<std::vector<std::string>> readCSV(const std::string& filename) {
+std::vector<std::vector<std::string>> readCSV(const std::string &filename) {
     // std::string filename2("D:\\data\\temperature_sensing_cable_V0\\data\\cable\\data_v2\\隧道火灾实验数据\\１、紧急停车带火灾实验\\6.25紧急停车带 1L汽油 线缆参数修改（18℃启开始差温检测）\\二代线 27s报警 20240625.CSV");
     std::ifstream file(filename);
 
@@ -30,20 +25,31 @@ std::vector<std::vector<std::string>> readCSV(const std::string& filename) {
         std::vector<std::string> row;
         std::stringstream ss(line);
         std::string cell;
-
         while (std::getline(ss, cell, ',')) {
             row.push_back(cell);
         }
-
         data.push_back(row);
     }
-
     return data;
 }
 
-int main(int argc, char *argv[])
-{
-    setbuf(stdout,NULL);
+
+int convertTimeStringToSeconds(const std::string &timeStr) {
+    int hours, minutes, seconds;
+    int microseconds = 0;
+    char delim;
+    std::istringstream iss(timeStr);
+    iss >> hours >> delim >> minutes >> delim >> seconds;
+    if (iss.peek() == '.') {
+        iss >> delim >> microseconds;
+    }
+    double totalSeconds = hours * 3600.0 + minutes * 60.0 + seconds + microseconds / 1e6;
+    return static_cast<int>(totalSeconds);
+}
+
+
+int main(int argc, char *argv[]) {
+    setbuf(stdout, NULL);
     printf("%s\n", "开始");
     cout << "开始" << endl;
 
@@ -61,65 +67,70 @@ int main(int argc, char *argv[])
     }
     std::vector<std::string> vec;
     std::string line;
-    while (std::getline(file, line)) { // 读取文件每一行
-        vec.push_back(line); // 将每一行添加到向量中
+    while (std::getline(file, line)) {
+        vec.push_back(line);
     }
     file.close();
 
-    // string csv_data_path = "D:/data/temperature_sensing_cable_V0/data/cable/data_v2/隧道火灾实验数据/１、紧急停车带火灾实验/6.22紧急停车带隧道火灾实验数据/6.22实验 1L汽油 紧急停车带 风速0.3－43S报警/20240622 保护套线原始数据.CSV";
     string csv_data_path = "/home/manu/tmp/cable_demo_parser_save/heta-cable_00000002333232373739393630353737_1_1_segment_0.csv";
-    // string csv_data_path = "../data/test.CSV";
-    // string csv_data_path = "D:/data/02_CableTemperatureDetector/log/heta-cable.2024-09-18.0.log15495187616985280.csv";
-    // string csv_data_path = "D:/data/02_CableTemperatureDetector/log/heta-cable.2024-09-17.0.log15408741163688121.csv";
-    // string csv_data_path = "D:/data/02_CableTemperatureDetector/log/heta-cable.2024-09-16.0.log15322323894386850.csv";
     std::vector<std::vector<std::string>> data_raw = readCSV(csv_data_path);
 
     int res = 0;
-    for (int i = 0; i < data_raw.size(); i++)
-    {
+    for (int i = 0; i < data_raw.size(); i++) {
         std::vector<std::string> one_line_data = data_raw[i];
         int8_t data[RAW_DATA_ONE_GROUP_LENGTH] = {0};
         int idx;
         int cable_idx = 0;
         std::string group_id_tmp = one_line_data[4];
+        std::string time_str = one_line_data[1];
         group_id_tmp.erase(std::remove_if(group_id_tmp.begin(), group_id_tmp.end(), ::isspace), group_id_tmp.end());
         int a = group_id_tmp.size();
-        std::string group_id2 = group_id_tmp.substr(1, group_id_tmp.size()-2);
-        if (i > 116)
-        {
-            int a = 0;
-            // data[46] = 95;
-        }
+
+        std::string group_id2 = group_id_tmp.substr(1, group_id_tmp.size() - 2);
         idx = stoi(group_id2);
         std::string target = "ALARM: ";
 
-        for (int j = 0;j < RAW_DATA_ONE_GROUP_LENGTH; j++)
-        {
-            size_t pos = one_line_data[6+j].find(target);
-            if (pos != std::string::npos)
-            {
-                one_line_data[6+j].replace(pos, target.length(), "");
+        const std::string time_str_min = "00:00:00.00";
+        const std::string time_str_max = "11:00:00.00";
+        int totalSecondsMin = convertTimeStringToSeconds(time_str_min);
+        int totalSecondsMax = convertTimeStringToSeconds(time_str_max);
+        int totalSeconds = convertTimeStringToSeconds(time_str);
+
+//        std::cout << "Total integer seconds: " << totalSeconds << std::endl;
+//        std::cout << "Total integer min seconds: " << totalSecondsMin << std::endl;
+
+        if (totalSeconds < totalSecondsMin) {
+            std::cout << "skipping ..." << std::endl;
+            continue;
+        }
+
+        if (totalSeconds > totalSecondsMax) break;
+        std::cout << "time_str -->" << time_str << std::endl;
+
+        for (int j = 0; j < RAW_DATA_ONE_GROUP_LENGTH; j++) {
+            size_t pos = one_line_data[6 + j].find(target);
+            if (pos != std::string::npos) {
+                one_line_data[6 + j].replace(pos, target.length(), "");
             }
-            data[j] = stoi(one_line_data[6+j]);
+            data[j] = stoi(one_line_data[6 + j]);
         }
 
         int8_t use_ai_model = 1;
         // if (idx == 0)
         {
             // printf("process :%d, group:%s \n", i, group_id_tmp.c_str());
-            res = alg_cable_temperature_detector_run(data, idx*64, cable_idx, i, alarmList, use_ai_model);
+            res = alg_cable_temperature_detector_run(data, idx * 64, cable_idx, totalSeconds, alarmList, use_ai_model);
             //printf("********* [[%d]], %s %s, group:%s, res: %d *********\n", i, one_line_data[0].c_str(), one_line_data[1].c_str(), group_id_tmp.c_str(), res);
-            if (res > 0)
-            {
-                for (int j = 0; j < res; j++)
-                {
-                    printf("********* [[%d]], group:%s, res: %d, alarm_type:%d, alarm_temp: %d, addr:%d *********\n", i, group_id_tmp.c_str(), res, alarmList[j].alarm_type, alarmList[j].alarm_temp, alarmList[j].addr, 0);
+            if (res > 0) {
+                for (int j = 0; j < res; j++) {
+                    printf("********* [[%d]], group:%s, res: %d, alarm_type:%d, alarm_temp: %d, addr:%d *********\n", i,
+                           group_id_tmp.c_str(), res, alarmList[j].alarm_type, alarmList[j].alarm_temp,
+                           alarmList[j].addr, 0);
                 }
             }
             printf("process :%d, res:%d \n", i, res);
         }
-        // if (i > 256) break;
-        if (res > 0) break;
+//        if (res > 0) break;
     }
     printf("program over");
     return 0;
